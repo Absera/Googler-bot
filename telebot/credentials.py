b73863af8e0683d66d6218f9ac5b9c90af764443
Absera <abseratemesgen@gmail.com>
2020-08-22 13:52:25 +0300

    sjkdj

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "BOT-TOKEN-GOES-HERE"
-bot_user_name = "BOT-USER-NAME"
-URL = "THE-HEROKU-APP-LINK"
+bot_token = "1213803243:AAFut0Io9H2FLWvpGCmXbwe5IO6snwCCZGA"
+bot_user_name = "http://t.me/randomwikiwiki_bot"
+URL = "https://googler-bot.herokuapp.com/"
