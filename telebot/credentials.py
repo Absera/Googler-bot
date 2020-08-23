535f6692d31b86b07ca21a98bcd8fca31b5c10c7
Absera <50250863+Absera@users.noreply.github.com>
2020-08-23 19:47:36 +0300

    kjhjhk

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,4 @@
 bot_token = "1396812975:AAHYDLQVgxLEGf9JeRIUyRVKhFKpS-Zm6gw"
 bot_user_name = "http://t.me/googoolerBot"
 URL = "https://googler-bot.herokuapp.com/"
+# URL = "https://127.0.0.1:5000/"
