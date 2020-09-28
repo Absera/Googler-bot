db48c0194a96aa2956689f5d2d38922406ba5302
Absera Temesgen <50250863+Absera@users.noreply.github.com>
2020-09-29 01:48:20 +0300

    Update credentials.py

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "1396812975:AAHYDLQVgxLEGf9JeRIUyRVKhFKpS-Zm6gw"
-bot_user_name = "http://t.me/googoolerBot"
-URL = "https://googler-bot.herokuapp.com/"
+bot_token = "<token>"
+bot_user_name = "<username>"
+URL = "<url>"
